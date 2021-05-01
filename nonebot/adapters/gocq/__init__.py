d81bcfabc740c6681069c671134d6906cce231f6
Jigsaw <j1g5aw@foxmail.com>
2021-05-01 20:40:36 +0800

    :truck: Rename cqhttp with gocq in nonebot.adapters

--- nonebot/adapters/gocq/__init__.py
+++ nonebot/adapters/gocq/__init__.py
@@ -1,13 +1,8 @@
 """
-CQHTTP (OneBot) v11 协议适配
+go-cqhttp 协议适配
 ============================
 
-协议详情请看: `CQHTTP`_ | `OneBot`_
-
-.. _CQHTTP:
-    https://github.com/howmanybots/onebot/blob/master/README.md
-.. _OneBot:
-    https://github.com/howmanybots/onebot/blob/master/README.md
+协议详情请看: https://docs.go-cqhttp.org/
 """
 
 from .event import *
